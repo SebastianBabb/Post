bdf064bf80b6a2cbf412f36c22f98798242ea7d4
andreybarsukov <andr3y3@gmail.com>
2016-02-10 20:59:07 -0800

    working on catalog, need to implement ProductReader first

--- src/Catalog.java
+++ src/Catalog.java
@@ -8,13 +8,37 @@
  *
  * @author andre_000
  */
+
 public class Catalog {
+    private static final int MAX_PRODUCTS = 100;
     
+    private Item products[];   //the store's products, MAX 100
+    private int numProducts;  //stores num products currently in catalog, also indicates new open space for addition of item
     
-    Item products[] = new Item[100];
-    int numProducts;
-    
+    //DEFAULT CONSTRUCTOR
     Catalog(){
+        this.products = new Item[MAX_PRODUCTS];
+        this.numProducts = 0;  //first available stop = 0
+    }
+    
+    //SECONDARY CONSTRUCTOR
+    Catalog(Item newProducts[], int newNumProducts){
+        
+        if(newNumProducts > 100){ //exceeding max products
+            System.out.println("ERROR: products list cannot exceed 100, will only take first 100.");
+        }
+        
+        //inseart new products list
+        for(int i = 0; i < MAX_PRODUCTS; i++){
+            this.products[i] = newProducts[i];
+            this.numProducts +=1;
+        }    
         
     }
+    
+    int addProduct(){
+        
+        return 1;
+    }
+    
 }
