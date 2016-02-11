8f605ee7ee0aa461eee4aa7c4baa5b0ddbc0a7e0
andreybarsukov <andr3y3@gmail.com>
2016-02-10 18:57:21 -0800

    added Class Item, Class Catalog

--- src/Catalog.java
+++ src/Catalog.java
@@ -3,12 +3,57 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package post1;
 
 /**
  *
  * @author andre_000
  */
 public class Catalog {
+    
+    
+    String description;
+    String UPC;
+    double price;
+    int ID;
+    
+    //DEFAULT CONSTRUCTOR
+    Catalog(){
+        
+        this.description = "no description";
+        this.UPC = "0000";
+        this.price = 0.00;
+        this.ID = 0;
+    }
+    
+    //SECONDARY CONSTRUCTOR
+    Catalog(String newDescr, String newUPC, double newPrice, int newID){
+        
+        this.description = newDescr;
+        this.UPC = newUPC;
+        this.price = newPrice;
+        this.ID = newID;
+        
+    }
+    
+    //GETTERS
+    String getItemDescription(){
+        
+        return this.description;
+    }
+    
+    String getItemUPC(){
+        
+        return this.UPC;
+    }
+    
+    double getItemPrice(){
+        
+        return this.price;
+    }
+    
+    int getItemID(){
+        
+        return this.ID;
+    }
     
 }
