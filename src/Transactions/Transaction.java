befd5fd9a9cf4fed7bbf4cd87f1f7ceca9e81d9c
jkrubin <jkrubin42@gmail.com>
2016-02-11 11:12:02 -0800

    added getters/setters to transaction class

--- src/Transactions/Transaction.java
+++ src/Transactions/Transaction.java
@@ -18,4 +18,29 @@ public class Transaction {
         this.payment = payment;
     }
         
+    Customer getCustomer(){
+        return this.customer;
+    }
+    
+    ItemList getItemList(){
+        return this.lineItem;
+    }
+    
+    Payment getPayment(){
+        return this.payment;
+    }
+    
+    void setCustomer(String newName){
+        this.customer = new Customer(newName);
+    }
+    
+    void setItemList(int[][] newList,int itemsInCart){
+        this.lineItem = new ItemList(newList,itemsInCart);
+    }
+    
+    void setPayment(String payType, int paymentData){
+        this.payment = new Payment(payType, paymentData);
+    }
+    
+    
 }
